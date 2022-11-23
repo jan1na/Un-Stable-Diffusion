a2dda4be647ce8f188fc756653a9b903a89387b5
jan1na <janina.fritzke@gmail.com>
2022-11-23 17:56:58 +0100

    test uploading results to wandb

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,7 +1,6 @@
 def save_list_to_file(list, file_path):
-    print(list)
     with open(file_path, 'w') as fp:
-        fp.write('\n'.join(list))
+        fp.write(''.join(list))
 
 
 def read_list_from_file(path: str):
