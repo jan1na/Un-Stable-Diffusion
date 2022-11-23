3ecc2447222276e34fe766412832578eeb11c542
jan1na <janina.fritzke@gmail.com>
2022-11-23 19:50:42 +0100

    image metrics

--- metrics/image_metrics.py
+++ metrics/image_metrics.py
@@ -7,7 +7,7 @@ from typing import List
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load('ViT-B/32', device)
 
-def image_cosine_similarity(image_array_0: List, image_array_1: List) -> List(float):
+def image_cosine_similarity(image_array_0: list, image_array_1: list) -> list(float):
     # Calculate features
     with torch.no_grad():
         image_features_0 = model.encode_image(image_array_0)
