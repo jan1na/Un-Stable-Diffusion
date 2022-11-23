1debed9db1ab58a28d7cd90efcfb607aeb475a70
jan1na <janina.fritzke@gmail.com>
2022-11-23 20:06:43 +0100

    image metrics

--- metrics/image_metrics.py
+++ metrics/image_metrics.py
@@ -7,14 +7,21 @@ from typing import List
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load('ViT-B/32', device)
 
-def image_cosine_similarity(image_array_0, image_array_1):
+def image_cosine_similarity(image_0, image_1) -> float:
     # Calculate features
     with torch.no_grad():
-        image_features_0 = model.encode_image(image_array_0)
-        image_features_1 = model.encode_image(image_array_1)
+        image_features_0 = model.encode_image(image_0)
+        image_features_1 = model.encode_image(image_1)
     
     image_features_0 /= image_features_0.norm(dim=-1, keepdim=True)
     image_features_1 /= image_features_1.norm(dim=-1, keepdim=True)
     cosine_similarity = 100.0 * image_features_0 @ image_features_1.T
     print("consine similarity:", cosine_similarity)
     return cosine_similarity
+
+
+def image_cosine_similarity(image_array_0: list, image_array_1: list) -> list(float):
+    cos_sim = []
+    for img_0, img_1 in zip(image_array_0, image_array_1):
+        cos_sim.append(image_cosine_similarity(img_0, img_1))
+    return cos_sim
