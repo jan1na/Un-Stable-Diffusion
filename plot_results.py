6ddd8cc02af2623230fcbb8a1ceea705fcd8f7e0
jan1na <janina.fritzke@gmail.com>
2022-12-17 17:13:22 +0100

    final

--- plot_results.py
+++ plot_results.py
@@ -30,6 +30,9 @@ def main():
     entity, project = args.entity, args.project  # set to your entity and project
     runs = api.runs(entity + "/" + project)
     print(runs)
+    run = api.run("janina/stable-diffusion/char")
+    print(run)
+    print(run.summary)
 
     summary_list, config_list, name_list = [], [], []
     for run in runs:
