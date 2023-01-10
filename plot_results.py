5a0025326a5b24d58930d46bddf1f5feb8893bbe
jan1na <janina.fritzke@gmail.com>
2023-01-10 02:07:20 +0100

    plot

--- plot_results.py
+++ plot_results.py
@@ -1,4 +1,3 @@
-import wandb
 import argparse
 
 
@@ -22,6 +21,7 @@ def create_parser():
 
 def main():
     args = create_parser()
+    """
 
     import pandas as pd
     import wandb
@@ -50,6 +50,38 @@ def main():
         #config_list.append(
         #    {k: v for k, v in run.config.items()
         #     if not k.startswith('_')})
+
+        # .name is the human-readable name of the run.
+        name_list.append(run.name)
+
+    runs_df = pd.DataFrame({
+        "summary": summary_list,
+        "config": config_list,
+        "name": name_list
+    })
+
+    runs_df.to_csv("project.csv")
+    
+    """
+
+    import pandas as pd
+    import wandb
+    api = wandb.Api()
+
+    # Project is specified by <entity/project-name>
+    runs = api.runs("janina/stable-diffusion")
+
+    summary_list, config_list, name_list = [], [], []
+    for run in runs:
+        # .summary contains the output keys/values for metrics like accuracy.
+        #  We call ._json_dict to omit large files
+        summary_list.append(run.summary._json_dict)
+
+        # .config contains the hyperparameters.
+        #  We remove special values that start with _.
+        config_list.append(
+            {k: v for k, v in run.config.items()
+             if not k.startswith('_')})
 
         # .name is the human-readable name of the run.
         name_list.append(run.name)
