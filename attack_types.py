6a07188a63e14b3692affea638a78727cdde80c4
jan1na <janina.fritzke@gmail.com>
2023-03-13 17:57:57 +0100

    homophone

--- attack_types.py
+++ attack_types.py
@@ -1,5 +1,5 @@
 file_names = ["original_control", "naive_char", "char", "delete_char", "duplicate_char", "synonym_word",
-              "homophone_word", "homophone-word_2"]
+              "homophone_word", "homophone_word_2"]
 run_names = ["original-control", "naive-char", "char", "delete-char", "duplicate-char", "synonym-word",
              "homophone-word", "homophone-word_2"]
 title_names = ['Original Control', 'Naive Char Permutation', 'Char Permutation', 'Delete Char Permutation',
