9ad06c25ab7f2df651de4ffeebf2aacffd3bce12
jan1na <janina.fritzke@gmail.com>
2023-03-13 20:18:16 +0100

    homoglyphs

--- attack_types.py
+++ attack_types.py
@@ -1,7 +1,7 @@
-file_names = ["original_control", "naive_char", "char", "delete_char", "duplicate_char", "synonym_word",
-              "homophone_word", "homophone_word_2"]
-run_names = ["original-control", "naive-char", "char", "delete-char", "duplicate-char", "synonym-word",
-             "homophone-word", "homophone-word_2"]
+file_names = ["original_control", "naive_char", "char", "delete_char", "duplicate_char", "typo_char",
+              "homoglyphs_char", "synonym_word", "homophone_word", "homophone_word_2"]
+run_names = ["original-control", "naive-char", "char", "delete-char", "duplicate-char", "typo-char",
+             "homoglyphs-char", "synonym-word", "homophone-word", "homophone-word_2"]
 title_names = ['Original Control', 'Naive Char Permutation', 'Char Permutation', 'Delete Char Permutation',
-               'Duplicate Char Permutation', 'Synonym Word Permutation', 'Homophone Word Permutation',
-               'Homophone Word Permutation 2']
+               'Duplicate Char Permutation', 'Typo Char Permutation', 'Homoglyphs Char Permutation',
+               'Synonym Word Permutation', 'Homophone Word Permutation', 'Homophone Word 2 Permutation']
