b20debc08ba82cfa1b2f64789d3e73fb9ca8c6d0
jan1na <janina.fritzke@gmail.com>
2023-03-25 17:43:44 +0100

    metrics

--- metrics/image_metrics.py
+++ metrics/image_metrics.py
@@ -109,6 +109,7 @@ def image_content_similarity(captions_path_0: str, captions_path_1: str) -> [flo
                                return_tensors="pt")
 
         text_embeddings = text_encoder(text_input.input_ids.to('cuda'))[0]
+        torch.cuda.empty_cache()
 
         caption_0_feature = torch.flatten(text_embeddings[0].unsqueeze(0), start_dim=1)
         caption_1_feature = torch.flatten(text_embeddings[1].unsqueeze(0), start_dim=1)
