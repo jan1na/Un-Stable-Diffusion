24ea53520e42e14f34f97943f986fb5460464fa5
jan1na <janina.fritzke@gmail.com>
2023-03-25 17:53:10 +0100

    metrics

--- metrics/image_metrics.py
+++ metrics/image_metrics.py
@@ -101,9 +101,10 @@ def image_content_similarity(captions_path_0: str, captions_path_1: str) -> [flo
 
     cos_sim = []
 
+    i=0
     for caption_0, caption_1 in zip(captions_0, captions_1):
-        print("caption_0", caption_0)
-        print("caption_1", caption_1)
+        print("iteration:", i)
+        i+=1
 
         text_input = tokenizer([caption_0, caption_1],
                                padding="max_length",
