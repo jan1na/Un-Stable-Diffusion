da09c7339241cdb2d9791b5db102db0a74857572
jan1na <janina.fritzke@gmail.com>
2023-04-12 19:41:08 +0200

    delete

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -44,8 +44,7 @@ def delete_empty_lines(path: str):
     :param path: path to text file
     """
     lst = []
-    print("_____________________________________________________________________________")
-    for line in load_list_from_file(path)[9999:10000]:
+    for line in load_list_from_file(path)[10000:10010]:
         lst.append(line.strip())
         print("'" + line + "'")
 
