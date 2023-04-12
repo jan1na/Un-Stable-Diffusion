03f9924ace780b73b8840b4454fd9f1e3f59ae92
jan1na <janina.fritzke@gmail.com>
2023-04-12 18:59:33 +0200

    delete

--- prepare_image_captions.py
+++ prepare_image_captions.py
@@ -4,5 +4,5 @@ from attack_types import file_names, CAPTION_PATH
 
 delete_empty_lines(CAPTION_PATH + '/original')
 
-for file_name in zip(file_names):
+for file_name in file_names:
     delete_empty_lines(CAPTION_PATH + "/" + file_name)
