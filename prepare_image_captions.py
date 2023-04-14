dcd3e1130705132597d997a63da11db00d8e3779
jan1na <janina.fritzke@gmail.com>
2023-04-14 16:59:32 +0200

    image_captions

--- prepare_image_captions.py
+++
@@ -1,8 +0,0 @@
-from utils.file_utils import delete_empty_lines
-from attack_types import file_names, CAPTION_PATH
-
-
-delete_empty_lines(CAPTION_PATH + '/original')
-
-for file_name in file_names:
-    delete_empty_lines(CAPTION_PATH + "/" + file_name)
